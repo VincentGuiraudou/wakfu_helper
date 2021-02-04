5d88fdd6ac355f582d1f324324379abdb8262abd
Glitch (wakfu-helper) <none>
2021-02-04 15:00:09 +0000

    activité finale v4

--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ bot.once("ready", () => {
 });
 
 bot.on("message", message => {
-  bot.user.setActivity("Wakfu", { type: "WATCHING" });
+  bot.user.setActivity("Wakfu", { type: "Playing" });
   if (message.content.startsWith(`${prefix}`)) {
     var cmd = message.content.substring(prefixLength).split(" ");
     switch (cmd[0]) {
